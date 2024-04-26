e8b40835a13b9ab647df463eb4100f00be986425
joseph valerio <jvalerio.200221@gmail.com>
2024-04-26 12:05:32 -0600

    test commit

---
+++ src/main/java/com/mycompany/dowhile/Dowhile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dowhile;
+
+/**
+ *
+ * @author joubr
+ */
+public class Dowhile {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        int test = 1;
+        
+        while(test==0){
+            System.out.println("while");
+        }
+        
+        do{
+            System.out.println("do while");
+        }while(test==0);
+    }
+}
